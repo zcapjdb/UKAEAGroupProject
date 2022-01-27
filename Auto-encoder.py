62ea66298fc0ce05de779451846fd219e9c71d7a
Thandi Madula <zcapmad@ucl.ac.uk>
2022-01-27 17:24:43 +0000

    encoder and decoder separate classes

--- Auto-encoder.py
+++ Auto-encoder.py
@@ -2,10 +2,10 @@ import numpy as np
 import pandas as pd 
 import torch.nn as nn
 
-class AutoEncoder(nn.Model):
-
-    def __init__(self):
+class Encoder(nn.Model): 
+    def __init__(self, latent_dims):
         super().__init__()
+        self.latent_dims = latent_dims
 
         self.encoder = nn.Sequential(
             nn.Linear(15,20),
@@ -13,12 +13,22 @@ class AutoEncoder(nn.Model):
             nn.Linear(20, 10),
             nn.ReLu(), 
             nn.Linear(10,5), 
-            nn.ReLu(), 
-            nn.Linear(5, 3),
+            nn.ReLu(),
+            nn.Linear(5, self.latent_dims),
         )
+    
+    def forward(self, x): 
+        encoded = self.encoder(x)
 
-        self.decoder = nn.Sequential(
-            nn.Linear(3,5),
+        return encoded
+
+class Decoder(nn.Model):
+        def __init__(self, latent_dims):
+            super().__init__()
+            self.latent_dims = latent_dims
+
+            self.decoder = nn.Sequential(
+            nn.Linear(latent_dims,5),
             nn.ReLu(), 
             nn.Linear(5, 10), 
             nn.ReLu(), 
@@ -27,8 +37,26 @@ class AutoEncoder(nn.Model):
             nn.Linear(20,15)
         )
 
-        def forward(self, x): 
-            encoded = self.encoder(x)
+        def forward(self, encoded):
             decoded = self.decoder(encoded)
+
             return decoded
+
+
+
+    
+
+class AutoEncoder(nn.Model):
+
+    def __init__(self, latent_dims, encoder, decoder):
+        super().__init__()
+        self.latent_dims = latent_dims
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def forward(self, x):
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+
+        return decoded
 
